030e3730d9a8ec2dbc09fad6557122a5ea65c3ab
husm <husm@pvc123.com>
2021-03-22 00:38:38 +0800

    为组件添加jsx语法

---
+++ Week14/jsx/main.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-21 23:55:16
+ * @LastEditTime: 2021-03-22 00:20:35
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /Frontend-07-Template/Week14/jsx/main.js
+ */
+for(let i of [1, 2, 3]){
+    console.log(i);
+}
+
+let a = <div></div>
--- Week14/jsx/webpack.config.js
+++ Week14/jsx/webpack.config.js
@@ -1,25 +1,13 @@
-<!--
+/*
  * @Author: your name
- * @Date: 2020-12-01 22:30:53
- * @LastEditTime: 2021-03-22 00:24:24
+ * @Date: 2021-03-21 23:51:47
+ * @LastEditTime: 2021-03-22 00:19:54
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
- * @FilePath: /Frontend-07-Template/Week14/README.md
--->
-学习笔记
+ * @FilePath: /Frontend-07-Template/Week14/jsx/webpack.config.js
+ */
 
-
-# 为组件添加jsx语法:
-1. 安装全局 webpack webpack-cli
-2. 安装 webpack babel-loader
-3. 安装 @babel/core @babel/preset-env
-4. webpackconfig 配置
-5. 安装 @babel/plugin-transform-react-jsx
-
-
-## webpackconfig 配置：
-```
-module.exports = {
+ module.exports = {
     entry: "./main.js",
     module: {
         rules: [
@@ -37,4 +25,3 @@ module.exports = {
     },
     mode: "development"
 }
-```
